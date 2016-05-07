ffd96b5ae8020f1b455f42222a18ecb06f5617f2
cypherix93 <bikram.ghosh93@gmail.com>
2016-05-07 00:30:44 -0400

    Added  a db context to the app.

---
+++ src/app/core/database/context/DbContext.ts
@@ -0,0 +1,25 @@
+import path = require("path");
+import fs = require("fs");
+import lowdb = require("lowdb");
+import storage = require("lowdb/file-sync");
+
+import {Game} from "../models/Game";
+import {Review} from "../models/Review";
+import {Config} from "../../config/Config";
+
+export class DbContext
+{
+    private static dbLocation:string = path.join(Config.current.rootPath, "../data/db.json");
+
+    public static context = lowdb(DbContext.dbLocation, {storage}, false);
+
+    public static get games()
+    {
+        return DbContext.context("games") as Game[];
+    }
+
+    public static get reviews()
+    {
+        return DbContext.context("reviews") as Review[];
+    }
+}
