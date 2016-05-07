fe1557be419ec48b930ae492f8af52b81a151434
cypherix93 <bikram.ghosh93@gmail.com>
2016-05-06 22:07:14 -0400

    Fixed a bug with sass compilation.

--- gulp/compile/ui/preprocess-sass.js
+++ gulp/compile/ui/preprocess-sass.js
@@ -10,14 +10,12 @@ module.exports = function (gulp, plugins, paths)
         return gulp.src(paths.sass + "main.scss")
             .pipe(plugins.debug({title: "compiling sass:"}))
             .pipe(plugins.plumber())
-            .pipe(plugins.sourcemaps.init())
             .pipe(plugins.sassGlob())
             .pipe(plugins.sass())
             .pipe(gulp.dest(cssDir))
 
             .pipe(plugins.cleanCss())
             .pipe(plugins.rename({suffix: ".min"}))
-            .pipe(plugins.sourcemaps.write("./"))
             .pipe(gulp.dest(cssDir));
     });
 };
