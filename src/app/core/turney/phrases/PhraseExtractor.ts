7da80b8796fba8f97a1ae53da204b2b0ac7c71e1
cypherix93 <bikram.ghosh93@gmail.com>
2016-05-08 00:11:57 -0400

    Wrote more logic to be able to extract right phrases.

--- src/app/core/turney/phrases/PhraseExtractor.ts
+++ src/app/core/turney/phrases/PhraseExtractor.ts
@@ -1,4 +1,7 @@
 import {PhraseExtractorBase} from "./PhraseExtractorBase";
+import {Phrase} from "../../database/models/Phrase";
+import {POSTagger} from "../../tagger/POSTagger";
+import {Word} from "../../database/models/Word";
 
 export class PhraseExtractor extends PhraseExtractorBase
 {
@@ -7,5 +10,31 @@ export class PhraseExtractor extends PhraseExtractorBase
         super();
     }
     
+    public extract(sequence:string):Phrase[]
+    {
+        var taggedWords = POSTagger.tagSequence(sequence);
+        return this.extractPhrasesWithFeatures(taggedWords);
+    }
     
+    private extractPhrasesWithFeatures(taggedWords:Word[]):Phrase[]
+    {
+        var extractedPhrases:Phrase[] = [];
+        
+        for (let i = 0; i < taggedWords.length; i++)
+        {            
+            let phrase = new Phrase(taggedWords[i], taggedWords[i+1], taggedWords[i+2]);
+            
+            if(this.isMatchingPhrase(phrase))
+                extractedPhrases.push(phrase);
+        }
+        
+        return extractedPhrases;
+    }
+
+    private isMatchingPhrase(phrase:Phrase):boolean
+    {
+        // Implement the matching phrase by looking at each feature in this extractor
+
+        return false;
+    }
 }
