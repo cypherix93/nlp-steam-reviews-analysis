63c0433f0c8203fecc690df13134e25e22722ff8
cypherix93 <bikram.ghosh93@gmail.com>
2016-05-08 22:44:13 -0400

    Made stubs to compute polarity.

---
+++ src/app/core/turney/polarity/PolarWordPair.ts
@@ -0,0 +1,11 @@
+export class PolarWordPair
+{
+    public positive:string;
+    public negative:string;
+
+    constructor(positive:string, negative:string)
+    {
+        this.positive = positive;
+        this.negative = negative;
+    }
+}
--- src/app/core/turney/polarity/PolarityCalculator.ts
+++ src/app/core/turney/polarity/PolarityCalculator.ts
@@ -1,5 +1,10 @@
 import {Phrase} from "../../database/models/Phrase";
+import {PolarWordPair} from "./PolarWordPair";
 
-export class PolarityCalculator {
-
+export class PolarityCalculator
+{
+    public static computePolarity(phrase:Phrase, polarWordPair:PolarWordPair):number
+    {
+        return -1;
+    }
 }
