2019e6981e519ff1b41842bd3f032d3dbec0393e
cypherix93 <bikram.ghosh93@gmail.com>
2016-05-15 15:16:25 -0400

    Added new fields to game and review recommendation.

--- src/server/core/database/models/training/ReviewRecommendation.ts
+++ src/server/core/database/models/training/ReviewRecommendation.ts
@@ -1,9 +1,15 @@
 import {Model} from "../../../base/Model";
+import {Phrase} from "../Phrase";
 
 export class ReviewRecommendation extends Model
 {
     public reviewId:string;
+    public gameId:string;
+
     public recommended:boolean;
+
+    public phrases:Phrase[];
+    public polarity:number;
 
     constructor(reviewId:string, recommended:boolean)
     {
